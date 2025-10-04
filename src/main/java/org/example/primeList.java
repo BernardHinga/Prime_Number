01d8efabf58f51d10673d691532ea149241c07b3
Hinga Bernard <hinga.bernardn@gmail.com>
2025-10-04 02:22:08 +0100

    Clean up the output

--- src/main/java/org/example/primeList.java
+++ src/main/java/org/example/primeList.java
@@ -3,8 +3,9 @@ package org.example;
 import java.util.ArrayList;
 
 public class primeList {
-    private int num;
+    int num;
 
+//    Constructor
     public primeList(int n) {
         this.num = n;
     }
@@ -12,8 +13,12 @@ public class primeList {
         Prime prime = new Prime();
         ArrayList<Integer> nList = new ArrayList<Integer>();
 
+/* Since 2 is already a prime number and already accounted for in
+ * the main class, we can use it to initialize the array
+ */
         nList.add(2);
 
+//        Loop through all odd numbers and check which are prime, storing them in nlist.
         for (int i = 3; i < num; i +=2) {
             prime.setNumber(i);
 
@@ -22,7 +27,11 @@ public class primeList {
             }
         }
 
-        System.out.print(nList);
+        System.out.print("Prime numbers less than " + num + " are:\n");
+//        Print out the list
+        for (int i : nList) {
+            System.out.print(i + ", ");
+        }
     }
 
 }
